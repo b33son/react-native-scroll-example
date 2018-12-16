c7162f39be69b52869c323e74088ece09b3b5c2d
b33 <b33red@gmail.com>
2018-12-16 15:27:01 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Demo from "./src/Demo";
+export default class App extends React.Component {
+  render() {
+    return <Demo />;
+  }
+}
